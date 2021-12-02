80271a59b2b8254b5c254eb54254eaa5d96bec78
Brian Taylor <brian.taylor@bolderflight.com>
2021-12-01 18:12:40 -0700

    Eigen 3.4.0. Updated directory structure to support Arduino in addition to CMake builds.

---
+++ src/eigen.h
@@ -0,0 +1,39 @@
+/*
+* Brian R Taylor
+* brian.taylor@bolderflight.com
+* 
+* Copyright (c) 2021 Bolder Flight Systems Inc
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the “Software”), to
+* deal in the Software without restriction, including without limitation the
+* rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+* sell copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+* IN THE SOFTWARE.
+*/
+
+/*
+* Guarantee that the Eigen code is licensed
+* under the MPL2 and possibly more permissive licenses (like BSD)
+*/
+#define EIGEN_MPL2_ONLY
+
+/* Disable asserts to speed up runtime */
+#define EIGEN_NO_DEBUG 1
+
+/* Set vector registers */
+#define EIGEN_ARCH_DEFAULT_NUMBER_OF_REGISTERS 16
+
+/* Include core header */
+#include "Eigen/Core"
